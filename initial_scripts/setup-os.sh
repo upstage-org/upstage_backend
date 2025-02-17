26c299038c83cb232b471b564d2df1d16295a6a1
root <root@svc-dev-app2>
2025-02-17 01:56:07 +0000

    latest

--- initial_scripts/setup-os.sh
+++ initial_scripts/setup-os.sh
@@ -7,11 +7,12 @@ apt install ufw
 ufw allow ssh
 ufw enable
 
-# Setup Docker
+cp initial_scripts/logrotate_for_docker.commands /etc/logrotate.d/docker
+logrotate -d /etc/logrotate.d/docker
 
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt install ca-certificates curl logrotate
+
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -21,18 +22,20 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt update
 
 $(. /etc/os-release && echo "$VERSION_CODENAME")
 
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove ; done
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo docker run hello-world
 
 # Setup Nginx
 
 sudo apt update
+sudo apt upgrade
+
 sudo ufw app list
 sudo ufw allow 'Nginx Full'
 sudo ufw delete allow 'Nginx HTTP'
