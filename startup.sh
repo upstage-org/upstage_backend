c9a7c4a82c069275aaf4b309c649fa4691c27ebc
root <root@dev-app2>
2025-02-24 04:35:52 +0000

    latest

--- startup.sh
+++ startup.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 alembic upgrade head
-tail -f /dev/null
-
-#uvicorn src.main:app --proxy-headers --forwarded-allow-ips='*' --host 0.0.0.0 --port 3000 --reload
+uvicorn src.main:app --proxy-headers --forwarded-allow-ips='*' --host 0.0.0.0 --port 3000 --reload
