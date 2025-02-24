0a88aab0d437531309ad79ed38942dfa9fe060b1
root <root@dev-app2>
2025-02-24 03:05:05 +0000

    latest

--- initial_scripts/setup-your-domain.sh
+++ initial_scripts/setup-your-domain.sh
@@ -72,13 +72,18 @@ Completed service container setup."
 	2) sed "s/YOUR_DOMAIN_NAME/$dname/g" ./initial_scripts/nginx_templates/nginx_template_for_app_machines.conf >/etc/nginx/sites-available/$dname.conf
            mkdir -p /app_code/demo
            mkdir -p /app_code/uploads
+           mkdir -p /app_code/ruff_cache
+           chmod 777 /app_code/ruff_cache
            cp -r ./src /app_code
            cp -r ./scripts /app_code
            cp -r ./dashboard/demo /app_code
            cp -r ./requirements.txt /app_code
+           cp -r ./alembic.ini /app_code
            cp -r ./startup.sh /app_code
 
 	   read -p "Now is the perfect time to copy your load_env.py file generated on your service machine (most likely here: /root/upstage_backend/src/global_config ) to /app_code/src/global_config on this machine. Once this is done, press enter to continue: " ready
+           chmod 755 /app_code/src/global_config/load_env.py
+
 	   cd ./app_containers && ./run_docker_compose.sh 
 		;;
 	3) sed "s/YOUR_DOMAIN_NAME/$dname/g" ./initial_scripts/nginx_templates/nginx_template_for_streaming_machines.conf >/etc/nginx/sites-available/$dname.conf
--- startup.sh
+++ startup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export RUFF_CACHE_DIR=./ruff_cache
+
 alembic upgrade head
 ruff format src
 uvicorn src.main:app --proxy-headers --forwarded-allow-ips='*' --host 0.0.0.0 --port 3000 --reload
