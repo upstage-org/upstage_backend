f0de9cf6b1ddf5d85fac4914bdb3980d2c5f55e0
root <root@dev-app2>
2025-02-24 03:43:21 +0000

    latest

--- startup.sh
+++ startup.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-export RUFF_CACHE_DIR=./ruff_cache
-
 alembic upgrade head
-ruff format src
 uvicorn src.main:app --proxy-headers --forwarded-allow-ips='*' --host 0.0.0.0 --port 3000 --reload
