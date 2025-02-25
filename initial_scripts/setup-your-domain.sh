9b26c3f2f41ec68ed5492069f60ac9102bba3193
root <root@svc-dev-app2>
2025-02-25 01:26:58 +0000

    latest

--- initial_scripts/setup-your-domain.sh
+++ initial_scripts/setup-your-domain.sh
@@ -18,8 +18,8 @@ echo '#!/bin/bash' > $run_these_ufw_commands
 
 if [[ -z "$dname" ]]
 then
-	echo "No empty values allowed."
-	exit -1
+        echo "No empty values allowed."
+        exit -1
 fi
 
 # Setup Nginx and Certbot
@@ -40,7 +40,7 @@ openssl dhparam -out dhparam.pem 2048
 cd /etc/nginx/sites-available
 echo "
 server {
-	server_name $dname;
+        server_name $dname;
 } " > ${dname}.conf
 
 cd /etc/nginx/sites-enabled
@@ -55,7 +55,7 @@ certbot --nginx -d $dname
 cd $currdir
 
 case $machinetype in
-	1) sed "s/YOUR_DOMAIN_NAME/$dname/g" ./initial_scripts/nginx_templates/nginx_template_for_svc_machines.conf >/etc/nginx/sites-available/$dname.conf
+        1) sed "s/YOUR_DOMAIN_NAME/$dname/g" ./initial_scripts/nginx_templates/nginx_template_for_svc_machines.conf >/etc/nginx/sites-available/$dname.conf
            mkdir -p /postgresql_data/var
            mkdir -p /postgresql_data/data
            mkdir -p /mongodb_data_volume
@@ -67,17 +67,19 @@ case $machinetype in
            cp ./container_scripts/mqtt_server/pw.txt /mosquitto_files/etc/mosquitto/pw.txt
            cp ./container_scripts/mqtt_server/local_mosquitto.conf /mosquitto_files/etc/mosquitto/conf.d/local_mosquitto.conf
            cp ./container_scripts/mqtt_server/add_mqtt_cert_crontab.sh /mosquitto_files/etc/mosquitto/cron/add_mqtt_cert_crontab.sh
-	   cd ./service_containers && ./run_docker_compose.sh 
+           cd ./service_containers && ./run_docker_compose.sh 
 
-	   # Mosquitto inbound ports. Mosquitto is password-protected. These need to be open to the world.
-	   ufw allow 1884/any
-	   ufw allow 9002/any
+           # Mosquitto inbound websocket port. Mosquitto is password-protected. 
+           # The websocket needs to be open to the world, since the front end attaches to it.
+           # The TCP port is private, only used by our app server.
+           ufw allow 1884/any
+           ufw allow 9002/any
 
-	   cd $currdir
-	   echo "
+           cd $currdir
+           echo "
 Completed service container setup."
-		;;
-	2) sed "s/YOUR_DOMAIN_NAME/$dname/g" ./initial_scripts/nginx_templates/nginx_template_for_app_machines.conf >/etc/nginx/sites-available/$dname.conf
+                ;;
+        2) sed "s/YOUR_DOMAIN_NAME/$dname/g" ./initial_scripts/nginx_templates/nginx_template_for_app_machines.conf >/etc/nginx/sites-available/$dname.conf
            mkdir -p /app_code/demo
            mkdir -p /app_code/uploads
            cp -r ./src /app_code
@@ -97,20 +99,20 @@ Note that on Digital Ocean, the third IP in the 'hostname -I' command: ${arr[2]}
            echo "ufw allow from $SVC_HOST proto tcp to any port 5433 " >> $run_these_ufw_commands
            echo "ufw allow from $SVC_HOST proto tcp to any port 27018 " >> $run_these_ufw_commands
 
-	   read -p "
+           read -p "
 Now is the perfect time to copy your load_env.py file generated on your service machine (most likely here: /root/upstage_backend/src/global_config ) to /app_code/src/global_config on this machine. Once this is done, press enter to continue: " ready
            chmod 755 /app_code/src/global_config/load_env.py
 
-	   read -p "
+           read -p "
 Run the contents of this script over on the service machine:
-	   `cat $run_these_ufw_commands` 
-	   Hit enter when finished: " ready
-	   cd ./app_containers && ./run_docker_compose.sh 
-		;;
-	3) sed "s/YOUR_DOMAIN_NAME/$dname/g" ./initial_scripts/nginx_templates/nginx_template_for_streaming_machines.conf >/etc/nginx/sites-available/$dname.conf
-		;;
-	4) sed "s/YOUR_DOMAIN_NAME/$dname/g" ./initial_scripts/nginx_templates/nginx_template_for_front_end_machines.conf >/etc/nginx/sites-available/$dname.conf
-		;;
-	*) echo "No match for machine type $machinetype, exiting."
-		;;
+           `cat $run_these_ufw_commands` 
+           Hit enter when finished: " ready
+           cd ./app_containers && ./run_docker_compose.sh 
+                ;;
+        3) sed "s/YOUR_DOMAIN_NAME/$dname/g" ./initial_scripts/nginx_templates/nginx_template_for_streaming_machines.conf >/etc/nginx/sites-available/$dname.conf
+                ;;
+        4) sed "s/YOUR_DOMAIN_NAME/$dname/g" ./initial_scripts/nginx_templates/nginx_template_for_front_end_machines.conf >/etc/nginx/sites-available/$dname.conf
+                ;;
+        *) echo "No match for machine type $machinetype, exiting."
+                ;;
 esac
--- scripts/start_upstage.sh
+++ scripts/start_upstage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-alembic upgrade head
 cd /usr/app/
+alembic -c ./scripts/alembic.ini upgrade head
 uvicorn src.main:app --proxy-headers --forwarded-allow-ips='*' --host 0.0.0.0 --port 3000
 #tail -f /dev/null
