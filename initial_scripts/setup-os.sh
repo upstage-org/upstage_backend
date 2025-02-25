c1d5148119d5e1cf3b8c3481b75157f65d159776
root <root@svc-dev-app2>
2025-02-25 00:03:29 +0000

    latest

--- initial_scripts/setup-os.sh
+++ initial_scripts/setup-os.sh
@@ -1,37 +1,39 @@
 # #!/bin/bash
 # Install and setup the OS
-sudo apt update
-sudo apt upgrade
+export DEBIAN_FRONTEND=noninteractive
 
-apt install ufw
-ufw allow ssh
-ufw enable
+apt -y update
+apt -y upgrade
+
+apt -y install ufw
+yes | ufw allow ssh
+yes | ufw enable
 
 # Add Docker's official GPG key and other tools:
-sudo apt install ca-certificates curl logrotate
+apt -y install ca-certificates curl logrotate
 
 cp initial_scripts/logrotate_for_docker.commands /etc/logrotate.d/docker
 logrotate -d /etc/logrotate.d/docker
 
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt -y update
 
 $(. /etc/os-release && echo "$VERSION_CODENAME")
 
-for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove ; done
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do apt-get -y remove ; done
+apt -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-sudo docker run hello-world
+docker run hello-world
 
-sudo ufw status
+ufw status
 
 echo "
 Before proceeding to the next script, be sure that you have a domain name registered with a DNS registrar like namecheap, gandi, etc.
