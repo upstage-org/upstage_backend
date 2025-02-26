cab47540b90aefa41fad659b485ca05d216dbd08
root <root@svc-dev-app2>
2025-02-26 04:09:18 +0000

    latest

--- scripts/run_event_archive.py
+++ scripts/run_event_archive.py
@@ -7,16 +7,16 @@ if projdir not in sys.path:
     sys.path.append(appdir)
     sys.path.append(projdir)
 
-from src.global_config import MQTT_USER, MQTT_PASSWORD, MQTT_BROKER, MQTT_PORT
+from src.global_config import MQTT_ADMIN_USER, MQTT_ADMIN_PASSWORD, MQTT_BROKER, MQTT_ADMIN_PORT
 from src.event_archive.systems.system import run
 from src.event_archive.messages.mqtt import build_client
 
 if __name__ == "__main__":
-    print(f"Connecting to {MQTT_BROKER}:{MQTT_PORT} as {MQTT_USER}, {MQTT_PASSWORD}")
+    print(f"Connecting to {MQTT_BROKER}:{MQTT_ADMIN_PORT} as {MQTT_ADMIN_USER}, {MQTT_ADMIN_PASSWORD}")
     run()
     print('Running event archive')
     mqtt_client = build_client()
     print('Built client successful')
-    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-    mqtt_client.connect(MQTT_BROKER, MQTT_PORT)
+    mqtt_client.username_pw_set(MQTT_ADMIN_USER, MQTT_ADMIN_PASSWORD)
+    mqtt_client.connect(MQTT_BROKER, MQTT_ADMIN_PORT)
     mqtt_client.loop_forever()
--- scripts/run_upstage_stats.py
+++ scripts/run_upstage_stats.py
@@ -1,10 +1,10 @@
-from src.global_config import MQTT_BROKER, MQTT_PORT, MQTT_USER, MQTT_PASSWORD
+from src.global_config import MQTT_BROKER, MQTT_ADMIN_PORT, MQTT_ADMIN_USER, MQTT_ADMIN_PASSWORD
 from src.upstage_stats.mqtt import build_client
 
 if __name__ == "__main__":
-    print(f"Connecting to {MQTT_BROKER}:{MQTT_PORT} as {MQTT_USER}, {MQTT_PASSWORD}")
+    print(f"Connecting to {MQTT_BROKER}:{MQTT_ADMIN_PORT} as {MQTT_ADMIN_USER}, {MQTT_ADMIN_PASSWORD}")
     client = build_client()
     print('Built client successful')
-    client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
-    client.connect(MQTT_BROKER, MQTT_PORT)
+    client.username_pw_set(MQTT_ADMIN_USER, MQTT_ADMIN_PASSWORD)
+    client.connect(MQTT_BROKER, MQTT_ADMIN_PORT)
     client.loop_forever()
