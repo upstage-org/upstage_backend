2bc7b8967450c216e1208b6561e624b80a7d9dad
Nguyen Van Quang <nguyenquang06012000@gmail.com>
2025-03-05 00:43:27 +0700

    fix: get stage detail

--- src/assets/db_migrations/0baf970d7eb2_insert_default_asset_types.py
+++ src/assets/db_migrations/0baf970d7eb2_insert_default_asset_types.py
@@ -6,16 +6,14 @@ from sqlalchemy.sql import table, column
 from sqlalchemy import String
 
 # revision identifiers, used by Alembic.
-revision: str = '0baf970d7eb2'
-down_revision: Union[str, None] = 'eb504467a5d7'
+revision: str = "0baf970d7eb2"
+down_revision: Union[str, None] = "eb504467a5d7"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 # Define the asset_type table
 asset_type_table = table(
-    'asset_type',
-    column('name', String),
-    column('file_location', String)  
+    "asset_type", column("name", String), column("file_location", String)
 )
 
 # List of default asset types
@@ -26,23 +24,26 @@ default_asset_types = [
     "curtain",
     "prop",
     "avatar",
-    "image"
+    "image",
 ]
+
 
 def upgrade() -> None:
     conn = op.get_bind()
     for asset_type in default_asset_types:
         result = conn.execute(
-            sa.select(asset_type_table.c.name).where(asset_type_table.c.name == asset_type)
+            sa.select(asset_type_table.c.name).where(
+                asset_type_table.c.name == asset_type
+            )
         ).fetchone()
 
         print(result)
 
         if not result:
             op.bulk_insert(
-                asset_type_table,
-                [{"name": asset_type, "file_location": ""}]
+                asset_type_table, [{"name": asset_type, "file_location": ""}]
             )
+
 
 def downgrade() -> None:
     conn = op.get_bind()
