825232442f7169d2224f863988c6206e97450880
Nguyen Van Quang <nguyenquang06012000@gmail.com>
2025-03-12 22:12:28 +0700

    fix: payload validation

--- src/authentication/http/validation.py
+++ src/authentication/http/validation.py
@@ -1,7 +1,8 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 
 
 class LoginInput(BaseModel):
-    token: str = Field(..., min_length=5, max_length=10000, default=None)
+    token: Optional[str] = Field(None, min_length=5, max_length=10000)
     username: str = Field(..., min_length=5, max_length=100)
     password: str = Field(..., min_length=8, max_length=256)
