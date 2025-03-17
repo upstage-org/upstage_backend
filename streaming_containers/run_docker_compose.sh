ce0ef0e12ec85cfffe2ca7a1abf4270850f62bf7
root <root@streaming-dev-app2>
2025-03-17 04:24:37 +0000

    latest

--- streaming_containers/run_docker_compose.sh
+++ streaming_containers/run_docker_compose.sh
@@ -5,6 +5,9 @@ set -a
 cd /streaming_files/jitsi-docker-jitsi-meet*
 source /streaming_files/config/envfile 
 
+# This is recreated by the docker config mount
+rm -rf /streaming_config/jitsi-meet-cfg/web/keys
+
 docker compose down
 docker compose rm -f
 docker compose up -d
