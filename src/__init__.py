4ef673a49e413ed6c46b81000231a241a0516187
root <root@dev-app2>
2025-03-31 15:42:33 +0000

    task: update docker compose

--- src/__init__.py
+++ src/__init__.py
@@ -4,9 +4,11 @@ import sys
 
 appdir = os.path.abspath(os.path.dirname(__file__))
 projdir = os.path.abspath(os.path.join(appdir, ".."))
+projdir2 = os.path.abspath(os.path.join(appdir, "../src"))
+
 if projdir not in sys.path:
     sys.path.append(appdir)
     sys.path.append(projdir)
-
+    sys.path.append(projdir2)
 from src import global_config
 #from src.main import app
