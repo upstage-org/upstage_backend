d20cb01a144549a4911755af6b8e52bd042d4e32
root <root@streaming-dev-app2>
2025-05-17 01:42:28 +0000

    latest

--- initial_scripts/post_install/jitsi-cert-cron-script.sh
+++ initial_scripts/post_install/jitsi-cert-cron-script.sh
@@ -5,3 +5,10 @@ cp /etc/letsencrypt/live/YOUR_DOMAIN_NAME/fullchain.pem /var/lib/prosody/auth.YO
 cp /etc/letsencrypt/live/YOUR_DOMAIN_NAME/privkey.pem /var/lib/prosody/YOUR_DOMAIN_NAME.key
 cp /etc/letsencrypt/live/YOUR_DOMAIN_NAME/privkey.pem /var/lib/prosody/auth.YOUR_DOMAIN_NAME.key
 
+cp /etc/letsencrypt/live/YOUR_DOMAIN_NAME/fullchain.pem /etc/prosody/certs/YOUR_DOMAIN_NAME.crt
+cp /etc/letsencrypt/live/YOUR_DOMAIN_NAME/fullchain.pem /etc/prosody/certs/auth.YOUR_DOMAIN_NAME.crt
+cp /etc/letsencrypt/live/YOUR_DOMAIN_NAME/privkey.pem /etc/prosody/certs/YOUR_DOMAIN_NAME.key
+cp /etc/letsencrypt/live/YOUR_DOMAIN_NAME/privkey.pem /etc/prosody/certs/auth.YOUR_DOMAIN_NAME.key
+
+chmod 640 /etc/prosody/certs/*key
+chmod 640 /var/lib/prosody/*key
