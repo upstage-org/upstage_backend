66599cb63ddba145f8e5fdf82cdc3b7cfbc6849e
gloriajw <gwcorresp01@gmail.com>
2026-02-04 20:34:29 -0500

    change last_login to latest_activity

---
+++ src/users/db_migrations/f8a1b2c3d4e5_rename_last_login_to_latest_activity.py
@@ -0,0 +1,33 @@
+"""Rename last_login to latest_activity
+
+Revision ID: f8a1b2c3d4e5
+Revises: eb504467a5d7
+Create Date: 2025-02-04
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+
+
+revision: str = "f8a1b2c3d4e5"
+down_revision: Union[str, None] = "eb504467a5d7"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.alter_column(
+        "upstage_user",
+        "last_login",
+        new_column_name="latest_activity",
+    )
+
+
+def downgrade() -> None:
+    op.alter_column(
+        "upstage_user",
+        "latest_activity",
+        new_column_name="last_login",
+    )
