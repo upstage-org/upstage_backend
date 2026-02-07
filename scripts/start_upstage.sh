7aa003db4c1937599c4a6afc29ad5624c32c3f3e
gloriajw <gwcorresp01@gmail.com>
2026-02-07 11:37:19 -0500

    alembic cleanup, performance length

--- scripts/start_upstage.sh
+++ scripts/start_upstage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cd /usr/app/
-alembic -c ./scripts/alembic.ini upgrade head
+alembic -c ./scripts/alembic.ini upgrade heads
 uvicorn src.main:app --proxy-headers --forwarded-allow-ips='*' --host 0.0.0.0 --port 3000
 #tail -f /dev/null
