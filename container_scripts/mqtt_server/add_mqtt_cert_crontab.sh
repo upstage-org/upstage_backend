80b23d6917da1e621cc1c1be5a657d6a572dac41
Gloria W <gloriajw66@gmail.com>
2026-03-09 11:28:44 +1300

    latest

--- container_scripts/mqtt_server/add_mqtt_cert_crontab.sh
+++ container_scripts/mqtt_server/add_mqtt_cert_crontab.sh
@@ -5,11 +5,7 @@ mkdir -p /etc/mosquitto/ca_certificates/
 chmod 755 /etc/mosquitto/ca_certificates/
 chown mosquitto:mosquitto /etc/mosquitto/ca_certificates/
 
-<<<<<<< HEAD
-cp -L /etc/letsencrypt/live/testing.upstage.live/* /etc/mosquitto/ca_certificates/
-=======
 cp -L /etc/letsencrypt/live/upstage.live/* /etc/mosquitto/ca_certificates/
->>>>>>> d40fd1a3f0cc86828d26ca48f03d44a3f79c7fe5
 chown mosquitto:mosquitto /etc/mosquitto/ca_certificates/*
 
 # crontab does not run in the mosquitto docker container.
