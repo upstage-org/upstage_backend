740e73c458c05e6e306fa1b0b60f6fa53364969f
gloriajw <gwcorresp01@gmail.com>
2026-04-23 10:21:03 -0400

    update

---
+++ service_containers/run_docker_compose_dev.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# This file is generated. Do not change the generated copy. It will be
+# overwritten.
+
+set -a
+
+export POSTGRES_PASSWORD=changeme
+export MONGO_INITDB_ROOT_PASSWORD=changeme
+
+echo "
+If you need to run this installation more than once, and generated passwords have changed,
+be sure to remove and recreate the /postgresql_data/* dirs.
+"
+
+docker compose -f docker-compose-services-dev.yaml -p upstage-services-dev down --remove-orphans
+#docker compose rm -f
+docker compose -f docker-compose-services-dev.yaml -p upstage-services-dev up -d
+docker compose -f docker-compose-services-dev.yaml -p upstage-services-dev ps
