0c30753ac02722490fd47edbc4cdc086af2c8cad
gloriajw <gwcorresp01@gmail.com>
2026-04-30 09:14:04 -0400

    build stuff

--- app_containers/run_docker_compose_dev.sh
+++ app_containers/run_docker_compose_dev.sh
@@ -9,10 +9,7 @@ export HOST_UID="$(id -u)"
 export HOST_GID="$(id -g)"
 
 export DOCKER_CLIENT_DEBUG=1
-export HARDCODED_HOSTNAME=testing.upstage.live
-
-thisdir=`pwd`
-cd .. && uv sync --no-dev && cd $thisdir
+export HARDCODED_HOSTNAME=dev.upstage.live
 
 cp -r ../pyproject.toml /app_code_dev
 cp -r ../uv.lock /app_code_dev
@@ -20,8 +17,10 @@ cp -r ../src /app_code_dev
 cp -r ../alembic /app_code_dev
 cp -r ../scripts /app_code_dev
 cp -r ../dashboard/demo /app_code_dev
-cp -r ../requirements.txt /app_code_dev
 cp -r ../migration_scripts /app_code_dev
+
+cd /app_code_dev
+uv sync --no-dev 
 
 docker compose -f docker-compose-dev.yaml -p docker-backend-dev down --remove-orphans
 docker compose rm -f
