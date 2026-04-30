a6765e6b64e8f577fb942e5c0ebdb3a77f8a63dd
gloriajw <gwcorresp01@gmail.com>
2026-04-30 09:21:09 -0400

    script cleanup

---
+++ scripts/run_scaffold_load.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+cd /usr/app
+VENV="${VIRTUAL_ENV:-/usr/app/.venv}"
+export VIRTUAL_ENV="$VENV"
+export PATH="${VENV}/bin:${PATH}"
+export PYTHONPATH="/usr/app/src${PYTHONPATH:+:$PYTHONPATH}"
+exec python -m scripts.run_scaffold_load
