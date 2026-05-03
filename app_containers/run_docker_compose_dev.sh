eb329eacd267248994a2214b1d1fbc17b8b02232
gloriajw <gwcorresp01@gmail.com>
2026-05-03 17:22:21 -0400

    latest

--- app_containers/run_docker_compose_dev.sh
+++ app_containers/run_docker_compose_dev.sh
@@ -11,16 +11,19 @@ export HOST_GID="$(id -g)"
 export DOCKER_CLIENT_DEBUG=1
 export HARDCODED_HOSTNAME=dev.upstage.live
 
-cp -r ../pyproject.toml /app_code_dev
-cp -r ../uv.lock /app_code_dev
-cp -r ../src /app_code_dev
-cp -r ../alembic /app_code_dev
-cp -r ../scripts /app_code_dev
-cp -r ../dashboard/demo /app_code_dev
-cp -r ../migration_scripts /app_code_dev
+sudo cp -r ../pyproject.toml /app_code_dev
+sudo cp -r ../uv.lock /app_code_dev
+sudo cp -r ../src /app_code_dev
+sudo cp -r ../alembic /app_code_dev
+sudo cp -r ../scripts /app_code_dev
+sudo cp -r ../dashboard/demo /app_code_dev
+sudo cp -r ../migration_scripts /app_code_dev
 
+currwd=`pwd`
 cd /app_code_dev
-uv sync --no-dev 
+sudo uv sync --no-dev 
+sudo chown -R 1000:1000 /app_code_dev
+cd $currwd
 
 docker compose -f docker-compose-dev.yaml -p docker-backend-dev down --remove-orphans
 docker compose rm -f
