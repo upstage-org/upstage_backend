ffc68f2240dc296208ff6488c3f9eae62c9b9b90
gloriajw <gwcorresp01@gmail.com>
2026-05-03 11:40:54 -0400

    latest

--- service_containers/run_docker_compose_dev.sh
+++ service_containers/run_docker_compose_dev.sh
@@ -25,7 +25,9 @@ POSTGRES_PASSWORD="${!var^^}"
 : "${POSTGRES_PASSWORD:?$var is not set or is empty}" || exit 1
 
 if [ ! -d "${MQ_DATA_DIR}" ]; then
+    echo "First time MQTT setup..."
     sudo mkdir -p ${MQ_DATA_DIR}/etc/mosquitto/ca_certificates && \
+        sudo mkdir -p ${MQ_DATA_DIR}/etc/mosquitto/http && \
         sudo mkdir -p ${MQ_DATA_DIR}/var/lib/mosquitto && \
         sudo cp -r ./deployment_config/etc_mosquitto/* ${MQ_DATA_DIR}/etc/mosquitto && \
         sudo chmod 700  ${MQ_DATA_DIR}/etc/mosquitto/ca_certificates &&
@@ -33,13 +35,16 @@ if [ ! -d "${MQ_DATA_DIR}" ]; then
     echo "Change the performance and admin passwords in this file: ${MQ_DATA_DIR}/etc/mosquitto/pw.backup"
 
     if [[ ! -z $SSL ]]; then
-      # We update mosquitto certs in a Let's Encrypt renenwal hook
-      # script on the host server itself.
-      sudo certbot certonly \
-       --webroot \
-       --webroot-path /var/www/html \
-       -d ${HARDCODED_HOSTNAME} \
-       --deploy-hook "./deployment_config/on_server/letsencrypt_deploy_hook.sh"
+        # We update mosquitto certs in a Let's Encrypt renenwal hook
+        # script on the host server itself.
+        sudo rm -f ${MQ_DATA_DIR}/etc/mosquitto/conf.d/local_mosquitto_nossl.conf
+        sudo certbot certonly \
+         --webroot \
+         --webroot-path /var/www/html \
+         -d ${HARDCODED_HOSTNAME} \
+         --deploy-hook "./deployment_config/on_server/letsencrypt_deploy_hook.sh"
+    else
+        sudo rm -f ${MQ_DATA_DIR}/etc/mosquitto/conf.d/local_mosquitto_ssl.conf
     fi
 
     exit 0
@@ -59,8 +64,8 @@ fi
 sudo chown -R 1883:1883 ${MQ_DATA_DIR}
 
 if [ ! -d "${PG_DATA_DIR}" ]; then
+    echo "First time Postgres setup..."
     sudo mkdir -p ${PG_DATA_DIR}
-    docker logs 
 fi
 sudo chown -R 999:999 ${PG_DATA_DIR}
 
@@ -71,9 +76,30 @@ docker compose -f ${DOCKERFILE} -p ${SERVICES} down --remove-orphans
 docker compose -f ${DOCKERFILE} -p ${SERVICES} up -d
 docker compose -f ${DOCKERFILE} -p ${SERVICES} ps
 
-firstrun_fail=`docker logs postgres_container_${SITE} 2>&1 | grep -i "permission\|initdb\|could not change permissions\|No such container"`
-if [[ ! -z $firstrun_fail ]]; then
+counter=0
+firstrun_fail=`docker logs postgres_container_${SITE} 2>&1 | grep -i "initdb\|could not change permissions\|No such container"`
+while [[ -n $firstrun_fail ]] && [ "$counter" -lt 3 ]
+do
+    echo "Restarting Postgres...${firstrun_fail}"
+    sleep 3
     docker compose -f ${DOCKERFILE} -p ${SERVICES} down --remove-orphans postgres
+    sudo chown -R 999:999 ${PG_DATA_DIR}
     docker compose -f ${DOCKERFILE} -p ${SERVICES} up -d postgres
     docker compose -f ${DOCKERFILE} -p ${SERVICES} ps
-fi
+    counter=$((counter + 1))
+    firstrun_fail=`docker logs postgres_container_${SITE} 2>&1 | grep -i "initdb\|could not change permissions\|No such container"`
+done
+
+counter=0
+firstrun_fail=`docker logs mosquitto_container_${SITE} 2>&1 | grep -i "Permission denied\|could not change permissions\|No such container"`
+while [[ -n $firstrun_fail ]] && [ "$counter" -lt 3 ]
+do
+    echo "Restarting Mosquitto...${firstrun_fail}"
+    sleep 3
+    docker compose -f ${DOCKERFILE} -p ${SERVICES} down --remove-orphans mosquitto
+    sudo chown -R 1883:1883 ${MQ_DATA_DIR}
+    docker compose -f ${DOCKERFILE} -p ${SERVICES} up -d mosquitto
+    docker compose -f ${DOCKERFILE} -p ${SERVICES} ps
+    counter=$((counter + 1))
+    firstrun_fail=`docker logs mosquitto_container_${SITE} 2>&1 | grep -i "Permission denied\|could not change permissions\|No such container"`
+done
