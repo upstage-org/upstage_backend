efdf4ea073c54509e38bbe9110fe6f634d6b3e20
gloriajw <gwcorresp01@gmail.com>
2026-05-05 09:45:03 -0400

    migration fixes

--- pycoverage-script.sh
+++ pycoverage-script.sh
@@ -1,5 +1,8 @@
+#!/bin/sh
 export PGPASSWORD="postgres"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+cd "$SCRIPT_DIR"
 psql -h localhost -p 5432 -U postgres -c "DROP DATABASE IF EXISTS upstage_test;"
 psql -h localhost -p 5432 -U postgres -c "CREATE DATABASE upstage_test;"
-alembic upgrade head
+alembic -c scripts/alembic.ini upgrade heads
 coverage run -m pytest
